c5db6befe8886235bd9e415318dc487eac0f1ac5
euhidaman <aman.derax20@gmail.com>
2022-02-03 21:07:01 +0530

    Completed till factorial

---
+++ src/com/recursion/increasingDecreasing.java
@@ -0,0 +1,9 @@
+package com.recursion;
+
+import java.util.Scanner;
+
+public class incrasingDecreasing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
